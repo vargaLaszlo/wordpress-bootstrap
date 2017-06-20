b99f6976a04700f0c50fc4854a88f9f5bc8fe849
vargaLaszlo <laszlo.varga@jaystack.com>
2017-06-20 13:48:44 +0200

    Custom array

--- snippets.php
+++ snippets.php
@@ -5,4 +5,18 @@ function is_blog () {
     return ( is_home() || is_archive() || is_author() || is_category() || is_single() || is_tag()) && 'post' == get_post_type();
 }
 
+// Custom post type query args, with custom taxonomy
+$args = array(
+    'post_type' => array( 'downloads' ),
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'tax_slug',
+            'field' => 'slug',
+            'terms' => 'term_slug' // For more term OR use array: array('term_slug1', 'term_slug2'), for AND multiply tax_query block
+        )
+    ),      
+    'posts_per_page' => 999,
+    'order'  => 'DESC'
+);
+
 ?>
